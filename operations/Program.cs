ae66c153f5aed4b89f779eb5dc190d1c1849f46c
Jamal Dawson <jamal.a.dawson@gmail.com>
2018-05-29 00:47:53 -0400

    first commit

---
+++ operations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            bool isStudent = false;
+
+            if (age <= 0)
+                Console.WriteLine("Invalid age");
+            else if (age >= 65)
+                Console.WriteLine("pay $7");
+            else if (age <= 12 && age > 0)
+                Console.WriteLine("pay $8");
+            else if (isStudent)
+                Console.WriteLine("pay $8");
+            else
+                Console.WriteLine("pay $10");
+
+        }
+    }
+}
